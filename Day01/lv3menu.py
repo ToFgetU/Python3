5ba49c02e737d3aa8aba9ca2128443103ce8def1
ToFgetU <505567802@qq.com>
2017-05-05 11:51:43 +0800

    python3
    python3

---
+++ Day01/lv3menu.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: PanFei Liu
+
+menu_dict = {
+    '家用电器': {
+        '电视': {
+            '合资品牌': {
+                '夏普': 'LCD-70SU665A 70寸',
+                '索尼': 'KD-55X7000SD 50寸',
+                '三星': 'UA55KUF30ZJXXZ 55寸'
+            },
+            '国产品牌': {
+                '海信': 'LED55EC520UA 55寸',
+                'TCL': 'D50A810 50寸',
+                '康佳': 'S55U 55寸'
+            },
+            '互联网品牌': {
+                '小米': 'L43M5-AZ 32寸',
+                '乐视': '超4 X50 50寸'
+            }
+        },
+        '空调': {
+            '壁挂式': {
+                '美的': '美的正1.5匹 变频',
+                '格力': '格力正1.5匹 变频'
+            },
+            '柜式': {
+                '美的': '美的正3匹 变频',
+                '格力': '格力正3匹 变频'
+            }
+        }
+    },
+    '电脑': {
+        '笔记本': {
+            '联想': {
+                '联想G480': '￥4880',
+                '联想Y480': '￥5480'
+            },
+            '华硕': {
+                '华硕顽石四代': '￥4699'
+            }
+        },
+        '台式': {
+            '品牌': {
+                '联想': '￥4180'
+            },
+            '组装机': {
+                '组装': '￥3880'
+            }
+        }
+    }
+}
+
+main_menu = menu_dict.keys()
+menu = menu_dict
+while True:
+    for m in main_menu:
+        print(m)
+    if menu.keys():
+        name = input("Please select:")
+        val = menu[name]
+    else:
+        input("Back at the next higher level :")
+
+    if isinstance(val, dict):
+        if name in menu.keys():
+            main_menu = menu[name].keys()
+            menu = menu[name]
+        else:
+            name = input("The menu $s what you choice does not exist, Please select again:"% name)
+    else:
+        print(menu)
+        result = input("Back at the next higher level or quit?(quit: Q/q)")
+        if result in "Qq":
+            break
+        else:
+            main_menu = menu_dict.keys()
+            menu = menu_dict
+
