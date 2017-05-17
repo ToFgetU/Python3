77e2fe30bf58b82114554b73d95ec9c29386cdd4
ToFgetU <505567802@qq.com>
2017-05-17 23:28:34 +0800

    Python3
    Python3

--- Work05/ATM/core/login.py
+++ Work05/ATM/core/login.py
@@ -2,8 +2,33 @@
 # -*- coding: utf-8 -*-
 # Author: PanFei Liu
 
-def atm_login():
-    pass
+def mode(t):
+    if t == 'admin':
+        return 'admin'
+    elif t == 'atm':
+        return atm
+    else:
+        return 'shopping'
 
-def shopping_login():
-    pass
+def login_auth(mode):
+    """用户登入选择装饰器"""
+    def out_wrapper(func):
+        def wrapper(*args, **kwargs):
+            if mode == 'admin':
+                return func(*args, **kwargs)
+            elif mode == 'atm':
+                return func(*args, **kwargs)
+            else:
+                return func(*args, **kwargs)
+        return  wrapper
+    return out_wrapper
+
+
+
+@login_auth(mode=mode)
+def user_login():
+    print("登入成功")
+
+t = 'admin'
+mode(t)
+user_login()
