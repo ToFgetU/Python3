7055fc025035bc75c017c315b3c99b2e19cbd5a5
ToFgetU <505567802@qq.com>
2017-05-18 17:38:07 +0800

    Python3
    Python3

--- Work05/ATM/core/login.py
+++ Work05/ATM/core/login.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Author: PanFei Liu
+import json
+def user_data(t):
+    """读取数据信息"""
+    if t == 'shopping':
+        with open("../data/shopping_user.json", "r", encoding='utf-8') as f:
+            account = json.loads(f.read())
+            return account
+    else:
+        with open("../data/atm_user.json", "r", encoding='utf-8') as f:
+            account = json.loads(f.read())
+            return account
 
 def mode(t):
     if t == 'admin':
         return 'admin'
     elif t == 'atm':
-        return atm
+        return 'atm'
     else:
         return 'shopping'
 
 def login_auth(mode):
-    """用户登入选择装饰器"""
+    """用户登入选择"""
     def out_wrapper(func):
         def wrapper(*args, **kwargs):
             if mode == 'admin':
@@ -24,11 +35,27 @@ def login_auth(mode):
     return out_wrapper
 
 
+def login_required(func):
+    """验证用户是否登录,如果用户密码错误超过三次提示错误信息并退出程序"""
+    def wrapper(*args, **kwargs):
+        if args[1] == '123':
+            return func(*args, **kwargs)
+        else:
+            exit("User is not authenticated.")
+    return wrapper
 
+@login_required
 @login_auth(mode=mode)
-def user_login():
-    print("登入成功")
+def user_login(name, password, mode):
+    account = user_data(mode)
+    if name in account:
+        if password == account[name]['password']:
+            print("%s 登入成功"% name, password, mode)
+    else:
+        print("User or password is not correct")
 
-t = 'admin'
-mode(t)
-user_login()
+# t = 'admin'
+# mode(t)
+# user_login()
+
+
--- Work05/ATM/core/main_demo.py
+++ Work05/ATM/core/main_demo.py
@@ -2,10 +2,42 @@
 # -*- coding: utf-8 -*-
 # Author: PanFei Liu
 
-from core import process
+import login
 
 def run():
-    '''
+    """
     This function will be called right a way when the program started, here handles the user interaction stuff
-    :return:
-    '''
+    """
+
+    def change_to(t):
+        """该函数的主要功能是选择目的地"""
+        login.mode(t)
+        username = input("请输入用户名: ")
+        password = input("请输入密码:")
+        login.user_login(username, password, login.mode(t))
+
+    print("""
+    你打算去哪？
+    -->1. 去银行
+    -->2. 去超市
+    """)
+
+    change = input("您的选择是: ")
+
+    if change.strip() == '1':
+        is_admin = input("您是管理员吗(Y/N) : ")
+        if is_admin.lower() == 'y':
+            t = 'admin'
+            change_to(t)
+        else:
+            t = 'atm'
+            change_to(t)
+    elif change.strip() == '2':
+        t = 'shopping'
+        change_to(t)
+    else:
+        print("您选择了其他地方，再见。。。")
+
+
+
+run()
