e02702aacd9a2bbd74c7b625caf669df589c8377
ToFgetU <505567802@qq.com>
2017-06-19 17:49:25 +0800

    Python3
    Python3

--- Day09/test.py
+++ Day09/test.py
@@ -4,3 +4,22 @@
 
 print('test')
 print('hello')
+
+import pickle,json
+
+info = '123123'
+
+p = pickle.dumps(info)
+
+print(info, p)
+
+p1 = pickle.loads(p)
+print(p1)
+
+# with open('test.txt', 'wb') as f:
+#     p = pickle.dump(info, f)
+
+
+with open('test.txt', 'wb') as f:
+    p = pickle.dumps(info)
+    f.write(p)
