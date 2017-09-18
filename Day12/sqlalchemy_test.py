42b39b4c034a886001f04809748b98eae88c90eb
ToFgetU <505567802@qq.com>
2017-09-18 17:32:44 +0800

    Python3
    Python3

--- Day12/sqlalchemy_test.py
+++ Day12/sqlalchemy_test.py
@@ -6,8 +6,10 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
 
-t_engine = create_engine("mysql+pymysql://root:123456@192.168.128.133:3306/learndb",
+# echo=True  打印详细信息
+t_engine = create_engine("mysql+pymysql://root:123456@192.168.128.133:3306/learndb?charset=utf8",
                                     encoding='utf-8', echo=True)
 # 生成ORM基类
 Base = declarative_base()
@@ -20,3 +22,26 @@ class Users(Base):
 
 # 创建表结构
 Base.metadata.create_all(t_engine)
+
+# 创建数据库的会话 session_class ,注意, 这里返回给session的是class, 不是实例
+Session_class = sessionmaker(bind=t_engine)
+
+# 创建实例 同 cursor
+session = Session_class()
+
+# 生成你要创建的数据对象
+user_obj = Users(name='test', password='123')
+
+# 在这还没有创建数据对象
+print('user_obj:', user_obj.name, user_obj.id)
+
+# 把要创建的数据统一加进 session 中， 统一创建
+# session.add(user_obj)
+
+# 统一提交, 创建数据
+session.commit()
+
+#  数据查询
+my_user = session.query(Users).filter_by(name='test').first()
+print(my_user)
+print(my_user.id, my_user.name, my_user.password)
